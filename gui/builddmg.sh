937b08e5107c41144c6c882a7fa19b12f91ef660
Cole Bardin <colebardin@Coles-MacBook-Pro-2.local>
2022-02-08 23:46:37 -0500

    changed filenames

--- gui/builddmg.sh
+++ gui/builddmg.sh
@@ -4,17 +4,17 @@ mkdir -p dist/dmg
 # Empty the dmg folder.
 rm -r dist/dmg/*
 # Copy the app bundle to the dmg folder.
-cp -r "dist/Hello World.app" dist/dmg
+cp -r "dist/Amongus.app" dist/dmg
 # If the DMG already exists, delete it.
-test -f "dist/Hello World.dmg" && rm "dist/Hello World.dmg"
+test -f "dist/Amongus.dmg" && rm "dist/Amongus.dmg"
 create-dmg \
-  --volname "Hello World" \
-  --volicon "Hello World.icns" \
+  --volname "Amongus" \
+  --volicon "amongus.icns" \
   --window-pos 200 120 \
   --window-size 600 300 \
   --icon-size 100 \
-  --icon "Hello World.app" 175 120 \
-  --hide-extension "Hello World.app" \
+  --icon "Amongus.app" 175 120 \
+  --hide-extension "Amongus.app" \
   --app-drop-link 425 120 \
-  "dist/Hello World.dmg" \
+  "dist/Amongus.dmg" \
   "dist/dmg/"
